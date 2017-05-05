11c01c3729253e33eafc3a3b037980a98096e7c5
Gazaunga <Gazaunga@gmail.com>
2017-05-05 10:53:33 -0400

    Create fonstrap.sh

---
+++ fonstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd ~
+touch .fonts
+cd .fonts
+wget http://int10h.org/oldschool-pc-fonts/download/ultimate_oldschool_pc_font_pack_v1.0.zip
+unzip ultimate_oldschool_pc_font_pack_v1.0.zip
+git clone https://github.com/tonsky/FiraCode.git
+git clone https://github.com/sunaku/tamzen-font.git
+git clone https://github.com/FortAwesome/Font-Awesome.git
+pacaur -S --noconfirm --noedit ttf-hack \
+  ttf-ms-fonts \
+  ttf-google-fonts-git \
+  adobe-source-code-pro-fonts \
+  ttf-ubuntu-font-family \
+  noto-fonts-emoji \
+  ttf-symbola \
+  font-mathematica \
+  ttf-mac-fonts \
+fc-cache -v
