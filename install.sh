0a53c218521b02a237a42301084d1646d06d54e6
Gazaunga <Gazaunga@gmail.com>
2017-05-05 10:49:05 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Functions #
+# Create a new directory and enter it
+mkd() { mkdir $1 && cd $1; }
+##
+sudo pacman -Syu --noconfirm -- needed
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+pacaur -S --noconfirm --noedit xmonad xmonad-contrib xmobar dmenu scrot \
+    cabal-install xcompmgr
+sudo cabal update
+cabal install --global yeganesh
+pacaur -S --noconfirm --noedit emacs neovim python2-neovim python-neovim \
+  rofi \
+  otf-font-awesome oblogout ttf-hack-powerline-git ttf-hack-ibx \
+  polybar \
+  dolphin dolphin-plugins kompare kdegraphics-thmbnailers kdesdk-thumbnailers ffmpegthumbs raw-thumbnailer taglib kde-thumbnailer-epub \
+  libreoffice \
+  feh \
+  smplayer smtube smplayer-themes smplayer-skins \
+  howdoi \
+  glances \
+  openshot \
+  leafpad \
+  lxappearance \
+  rxvt-unicode \
+  fontforge \
+  krita \
+  pandoc typora \
+  xfce4-taskmanager \
+  texlive-most \
+  darktable \
+curl -sLf https://spacevim.org/install.sh | bash
+
+cd
+mv .xmonad .xmonad.orig
+git clone https://github.com/Gazaunga/xmonad-config.git .xmonad
