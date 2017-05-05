3d4d3c5300a03b1866ac43cf81a0fbe18dda5c72
Gazaunga <Gazaunga@gmail.com>
2017-05-05 10:53:45 -0400

    Update fonstrap.sh

--- fonstrap.sh
+++ fonstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~
-touch .fonts
+mkdir .fonts
 cd .fonts
 wget http://int10h.org/oldschool-pc-fonts/download/ultimate_oldschool_pc_font_pack_v1.0.zip
 unzip ultimate_oldschool_pc_font_pack_v1.0.zip
