1c42e634682e4d000a809d512f42f5a9a9d61be9
Gazaunga <Gazaunga@gmail.com>
2017-05-07 10:51:03 -0400

    Update fontstrap.sh

--- bin/fontstrap.sh
+++ bin/fontstrap.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+source ~/rubydev/bin/utils/sh
 cd ~
 mkdir .fonts
 cd .fonts
+e_success "Old School fonts"
 wget http://int10h.org/oldschool-pc-fonts/download/ultimate_oldschool_pc_font_pack_v1.0.zip
 unzip ultimate_oldschool_pc_font_pack_v1.0.zip
+e_success "Fira fonts"
 git clone https://github.com/tonsky/FiraCode.git
+e_success "Tamzen fonts"
 git clone https://github.com/sunaku/tamzen-font.git
+e_success "Font Awesome!"
 git clone https://github.com/FortAwesome/Font-Awesome.git
+e_success "Some Google fonts"
 pacaur -S --noconfirm --noedit ttf-hack ttf-google-fonts-git \
   ttf-ms-fonts \
   ttf-google-fonts-git \
@@ -18,4 +24,5 @@ pacaur -S --noconfirm --noedit ttf-hack ttf-google-fonts-git \
   ttf-mac-fonts \
 fc-cache -fv
 cd ~/rubydev
+e_header "It's time to DEPLOY!"
 sh deploy.sh
