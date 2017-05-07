175e65e371fdf7ab40d08bcc11353c0ada423545
Gazaunga <Gazaunga@gmail.com>
2017-05-07 10:49:38 -0400

    Update rubystrap.sh

--- bin/rubystrap.sh
+++ bin/rubystrap.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+source ~/rubydev/bin/utils/sh
+e_success "Let's grab Ruby and ruby-docs"
 pacaur -S --noconfirm --noedit ruby rbenv ruby-docs
+e_success "Make sure rubygems are in your path!"
 echo "PATH='$(ruby -e 'print Gem.user_dir')/bin:$PATH'" >> ~/.bashrc
 echo "GEM_HOME=$(ls -t -U | ruby -e 'puts Gem.user_dir')" >> ~/.bashrc
 echo "GEM_PATH=$GEM_HOME" >> ~/.bashrc
@@ -18,5 +21,6 @@ git config --global core.safecrlf true
 sudo gem install --no-user-instal rails rake jekyll bundler
 bundle install --path vendor/bundle 
 sudo chmod +x ~/bin/fontstrap.sh
+e_success "I think we're ready for some cool fonts now"
 sh fontstrap.sh
 
