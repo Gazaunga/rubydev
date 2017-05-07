4ca0cf75930b4c48358aae97231719fa3f939572
Gazaunga <Gazaunga@gmail.com>
2017-05-07 10:48:30 -0400

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+source ~/rubydev/bin/utils/sh
 # Functions #
 # Create a new directory and enter it
 mkd() { mkdir $1 && cd $1; }
 ##
+e_success "Grabbing Xmonad"
 sudo pacman -Syu --noconfirm --needed
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 pacaur -S --noconfirm --noedit xmonad xmonad-contrib xmobar dmenu scrot \
     cabal-install xcompmgr
 sudo cabal update
 cabal install --global yeganesh
+e_success "Grabbing some useful programs"
 pacaur -S --noconfirm --noedit emacs neovim python2-neovim python-neovim \
   rofi \
   oblogout \
@@ -34,9 +37,11 @@ pacaur -S --noconfirm --noedit emacs neovim python2-neovim python-neovim \
   chromium \
   interrobang-git sublime-text \
   xbindkeys
+e_success "Building Neovim"
 curl -sLf https://spacevim.org/install.sh | bash
 cd ~
 touch .xbindkeysrc
 mv -f .xmonad .xmonad.orig
 git clone https://github.com/Gazaunga/xmonad-config.git .xmonad
+e_success "Time to rubystrap!"
 sh rubystrap.sh
